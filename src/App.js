16be47ed8e3976d266eaf7cc6688c0f3f797fa33
Aref <aref056a@gmail.com>
2023-02-26 21:12:57 -0400

    Fin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Board from 'react-trello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = require('./data.json');
+
+const handleDragStart = (cardId, laneId) => {
+	console.log('drag started');
+	console.log(`cardId: ${cardId}`);
+	console.log(`laneId: ${laneId}`);
+};
+
+const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
+	console.log('drag ended');
+	console.log(`cardId: ${cardId}`);
+	console.log(`sourceLaneId: ${sourceLaneId}`);
+	console.log(`targetLaneId: ${targetLaneId}`);
+};
+
+class App extends Component {
+	state = { boardData: { lanes: [] } };
+
+	setEventBus = (eventBus) => {
+		this.setState({ eventBus });
+	};
+
+	async componentWillMount() {
+		const response = await this.getBoard();
+		this.setState({ boardData: response });
+	}
+
+	getBoard() {
+		return new Promise((resolve) => {
+			resolve(data);
+		});
+	}
+
+	completeCard = () => {
+		this.state.eventBus.publish({
+			type: 'ADD_CARD',
+			laneId: 'COMPLETED',
+			card: {
+				id: 'Milk',
+				title: 'Buy Milk',
+				label: '15 mins',
+				description: 'Use Headspace app',
+			},
+		});
+		this.state.eventBus.publish({
+			type: 'REMOVE_CARD',
+			laneId: 'PLANNED',
+			cardId: 'Milk',
+		});
+	};
+
+	addCard = () => {
+		this.state.eventBus.publish({
+			type: 'ADD_CARD',
+			laneId: 'BLOCKED',
+			card: {
+				id: 'Ec2Error',
+				title: 'EC2 Instance Down',
+				label: '30 mins',
+				description: 'Main EC2 instance down',
+			},
+		});
+	};
+
+	shouldReceiveNewData = (nextData) => {
+		console.log('New card has been added');
+		console.log(nextData);
+	};
+
+	handleCardAdd = (card, laneId) => {
+		console.log(`New card added to lane ${laneId}`);
+		console.dir(card);
+	};
+
+	render() {
+		return (
+			<div className='App'>
+				<div className='App-header'>
+					<Board editable onCardAdd={this.handleCardAdd} data={this.state.boardData} draggable onDataChange={this.shouldReceiveNewData} eventBusHandle={this.setEventBus} handleDragStart={handleDragStart} handleDragEnd={handleDragEnd} style={{ backgroundColor: '#0f172a' }} cardStyle={{ backgroundColor: '#94a3b8' }} laneStyle={{ backgroundColor: '#7f1d1d' }} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
